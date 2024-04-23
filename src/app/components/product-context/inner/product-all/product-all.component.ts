656217bccd980107872d6f1b944e55d2cc43b237
Kasun Fernando <fernandokasun672@gmail.com>
2024-04-23 17:52:31 +0530

    3rd commit from pc

--- src/app/components/product-context/inner/product-all/product-all.component.ts
+++ src/app/components/product-context/inner/product-all/product-all.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatProgressBar} from "@angular/material/progress-bar";
+import {RouterLink} from "@angular/router";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Component({
   selector: 'app-product-all',
   standalone: true,
-  imports: [],
+    imports: [
+        CurrencyPipe,
+        MatIcon,
+        MatIconButton,
+        MatProgressBar,
+        NgForOf,
+        NgIf,
+        RouterLink
+    ],
   templateUrl: './product-all.component.html',
   styleUrl: './product-all.component.scss'
 })
-export class ProductAllComponent {
+export class ProductAllComponent implements OnInit{
+  loading = false;
+
+  products:any[] = [];
+
+  constructor(private db:AngularFirestore,
+              private storage:AngularFireStorage
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+
+    this.db.collection('products').get().subscribe(querySnapShot=>{
+      querySnapShot.forEach(doc=>{
+        this.products.push({id:doc.id, data:doc.data()});
+        this.loading = false;
+      })
+    })
+
+  }
+
+  deleteProduct(id:any, image:any){
+    if(confirm('Are You Sure?')){
+      this.db.collection('products').doc(id).delete();
+      this.storage.storage.refFromURL(image).delete();
+    }
+  }
 
 }
--- src/app/components/product-context/product-context.component.ts
+++ src/app/components/product-context/product-context.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-product-context',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    RouterLink
   ],
   templateUrl: './product-context.component.html',
   styleUrl: './product-context.component.scss'
