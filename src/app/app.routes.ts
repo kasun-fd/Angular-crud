c83cd81465ae4ea4697bfd69173e70444d21b131
Kasun Fernando <fernandokasun672@gmail.com>
2024-05-25 22:06:01 +0530

    404 page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {DashboardHomeComponent} from "./components/dashboard-home/dashboard-home.component";
 import {CustomerContextComponent} from "./components/customer-context/customer-context.component";
@@ -10,24 +10,32 @@ import {ProductNewComponent} from "./components/product-context/inner/product-ne
 import {ProductUpdateComponent} from "./components/product-context/inner/product-update/product-update.component";
 import {ProductAllComponent} from "./components/product-context/inner/product-all/product-all.component";
 import {OrderContextComponent} from "./components/order-context/order-context.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
-  {path:'', redirectTo:'/dashboard/home', pathMatch:'full'},
-  {path:'dashboard', component:DashboardComponent, children:[
-      {path:'', redirectTo:'/dashboard/home', pathMatch:'full'},
-      {path:'home', component:DashboardHomeComponent},
-      {path:'customers', component:CustomerContextComponent, children:[
-          {path:'', redirectTo:'/dashboard/customers/new', pathMatch:'full'},
-          {path:'new', component:CustomerNewComponent},
-          {path:'update/:id', component:CustomerUpdateComponent},
-          {path:'list', component:CustomerAllComponent}
-        ]},
-      {path:'products', component:ProductContextComponent, children:[
+  {path: '', redirectTo: '/dashboard/home', pathMatch: 'full'},
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      {path: '', redirectTo: '/dashboard/home', pathMatch: 'full'},
+      {path: 'home', component: DashboardHomeComponent},
+      {
+        path: 'customers', component: CustomerContextComponent, children: [
+          {path: '', redirectTo: '/dashboard/customers/new', pathMatch: 'full'},
+          {path: 'new', component: CustomerNewComponent},
+          {path: 'update/:id', component: CustomerUpdateComponent},
+          {path: 'list', component: CustomerAllComponent}
+        ]
+      },
+      {
+        path: 'products', component: ProductContextComponent, children: [
           {path: '', redirectTo: '/dashboard/products/new', pathMatch: 'full'},
-          {path:'new', component: ProductNewComponent},
+          {path: 'new', component: ProductNewComponent},
           {path: 'update/:id', component: ProductUpdateComponent},
           {path: 'list', component: ProductAllComponent}
-        ]},
-      {path: 'orders', component:OrderContextComponent}
-    ]}
+        ]
+      },
+      {path: 'orders', component: OrderContextComponent}
+    ]
+  },
+  {path:'**', component:PageNotFoundComponent}
 ];
